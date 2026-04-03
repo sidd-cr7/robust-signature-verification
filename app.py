2ce51b17137b2c35a8b25861faf722de8e751037
sidd-cr7 <sidd-cr7@users.noreply.github.com>
2026-04-03 12:21:46 +0530

    Add Dockerfile for Hugging Face Spaces

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def verify_siamese():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 7860))
     app.run(host='0.0.0.0', port=port)
